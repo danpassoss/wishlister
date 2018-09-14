8ab740e090201f612c6d683c67536e8b35c59859
danpassoss <dann.passos@hotmail.com>
2018-09-14 20:49:25 -0300

    Adjusment on prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ config :wishlister, WishlisterWeb.Endpoint,
 # Do not print debug messages in production
 config :logger, level: :info
 
-config :hello, Wishlister.Repo,
+config :wishlister, Wishlister.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
