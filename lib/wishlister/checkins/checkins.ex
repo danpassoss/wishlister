1618ebbdb3c2d5967a4630046c991086756e268d
danpassoss <dann.passos@hotmail.com>
2018-10-13 17:58:26 -0300

    Add Foursquare API module

--- lib/wishlister/checkins/checkins.ex
+++ lib/wishlister/checkins/checkins.ex
@@ -3,6 +3,7 @@ defmodule Wishlister.Checkins do
   import Ecto.Query
 
   alias Wishlister.{
+    Api.Foursquare,
     Checkins.Venue,
     Repo
   }
@@ -14,8 +15,7 @@ defmodule Wishlister.Checkins do
 
   def recents_friends_checkins(user_token) do
     user_token
-      |> recent_friends_checkin_suffix
-      |> make_request(user_token)
+      |> Foursquare.get_recent_friends_checkins
   end
 
   def add_venue_to_wishlist(user, venue) do
@@ -41,58 +41,4 @@ defmodule Wishlister.Checkins do
     select: %Venue{id: v.id, name: v.name, image_url: v.image_url, venue_pid: v.venue_pid}
   end
 
-  defp make_request(suffix, user_token) do
-    case HTTPoison.get("https://api.foursquare.com/v2/" <> suffix) do
-      {:ok, %HTTPoison.Response{body: body, status_code: 200}} ->
-        parse_response(body)
-        |> build_venue_friends_list(user_token)
-
-      {:error, %HTTPoison.Error{reason: reason}} ->
-        IO.inspect(reason) #Clean warning: A expression is always required...
-    end
-  end
-
-  defp parse_response(body) do
-    body
-    |> JSON.decode!()
-  end
-
-  defp build_venue_friends_list(%{"response" => %{"recent" => checkins}}, user_token) do
-      for checkin <- checkins do
-        %{
-          venue_pid: checkin["venue"]["id"],
-          name: checkin["venue"]["name"],
-          image_url: venue_img_request(checkin["venue"]["id"], user_token)
-        }
-      end
-  end
-
-  defp venue_img_request(venue_id, token) do
-    url = "https://api.foursquare.com/v2/venues/#{venue_id}/photos?oauth_token=#{token}&v=#{join_today_date()}"
-    case HTTPoison.get(url) do
-      {:ok, %HTTPoison.Response{body: body, status_code: 200}} ->
-        parse_response(body)
-        |> build_venue_img_url
-      {:error, %HTTPoison.Error{reason: reason}} ->
-        IO.inspect(reason) #Clean warning: A expression is always required...
-      end
-  end
-
-  defp build_venue_img_url(%{"response" => %{"photos" => %{"items" => items}}}) do
-    case length(items) do
-      0 ->
-        "http://placehold.it/300x300"
-      _ ->
-        "#{List.first(items)["prefix"]}300x300#{List.first(items)["suffix"]}"
-      end
-  end
-
-  defp join_today_date() do
-    {{y, m, d}, _rest} = :calendar.local_time()
-    "#{y}0#{m}#{d}"
-  end
-
-  defp recent_friends_checkin_suffix(token) do
-    "checkins/recent?oauth_token=#{token}&limit=6&v=#{join_today_date()}"
-  end
 end
--- lib/wishlister/external_apis/foursquare.ex
+++ lib/wishlister/external_apis/foursquare.ex
@@ -1,3 +1,74 @@
-defmodule Wishlister.Foursquare do
+defmodule Wishlister.Api.Foursquare do
+
+  defp base_url do
+    "https://api.foursquare.com/v2"
+  end
+
+  defp join_today_date() do
+    {{y, m, d}, _rest} = :calendar.local_time()
+    "#{y}0#{m}#{d}"
+  end
+
+  defp venue_img_path_url(venue_id, token) do
+    "/venues/#{venue_id}/photos?oauth_token=#{token}&v=#{join_today_date()}"
+  end
+
+  defp recent_friends_checkin_path_url(token) do
+    "/checkins/recent?oauth_token=#{token}&limit=6&v=#{join_today_date()}"
+  end
+  def get_recent_friends_checkins(user_token) do
+    user_token
+    |> recent_friends_checkin_path_url
+    |> get_request
+    |> build_venue_friends_list(user_token)
+  end
+
+  defp get_request(path_url) do
+    case HTTPoison.get(base_url() <> path_url) do
+      {:ok, %HTTPoison.Response{body: body, status_code: 200}} ->
+        parse_response(body)
+
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        # Clean warning: A expression is always required...
+        IO.inspect(reason)
+    end
+  end
+
+  defp parse_response(body) do
+    response = body
+      |> JSON.decode!()
+
+    {:ok, response}
+  end
+
+  defp build_venue_friends_list(%{"response" => %{"recent" => checkins}}, user_token) do
+    for checkin <- checkins do
+      %{
+        venue_pid: checkin["venue"]["id"],
+        name: checkin["venue"]["name"],
+        image_url: venue_img_request(checkin["venue"]["id"], user_token)
+      }
+    end
+  end
+
+  defp venue_img_request(venue_id, token) do
+    case get_request(venue_img_path_url(venue_id, token)) do
+      {:ok, body} ->
+        build_venue_img_url(body)
+
+      {:error, reason} ->
+        IO.inspect(reason)
+    end
+  end
+
+  defp build_venue_img_url(%{"response" => %{"photos" => %{"items" => items}}}) do
+    case length(items) do
+      0 ->
+        "http://placehold.it/300x300"
+
+      _ ->
+        "#{List.first(items)["prefix"]}300x300#{List.first(items)["suffix"]}"
+    end
+  end
 
 end
